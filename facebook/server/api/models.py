53bee7095c86939e4b262c7b01adafcaaf4d4a97
Rakib013 <mdrakib32661@gmail.com>
2022-02-04 13:22:24 +0600

    Start modeling user database

--- facebook/server/api/models.py
+++ facebook/server/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from matplotlib.pyplot import cla
 
-# Create your models here.
+
+class User(AbstractUser):
+    profile = models.ImageField(default="avatar.png", upload_to='user/profile/', blank=True, null=True)
+    coverPic = models.ImageField(default="cover.png", upload_to='user/cover/', blank=True, null=True)
+    nickName = models.CharField(max_length=200, blank=True, null=True)
+    bio = models.TextField(default="", blank=True, null=True)
+    email = models.EmailField(unique=True)
+    friends = models.ManyToManyField('self', blank=True)
+    requestedFriend = models.ManyToManyField('self', blank=True, related_name='requestedFriend')
+
+    def __str__(self):
+        return self.username
+
+    
+class Picture(models.Model):
+    image = models.ImageField(upload_to='user/pictures/', blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.image.name
